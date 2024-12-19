fe14838793e4089f80347c3629063b06ab024ff3
ahmed <you@example.com>
2024-12-19 11:07:20 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from 'app/app.component';
+import { appConfig } from 'app/app.config';
+import { environment } from '../environment.prod'; 
+
+if (environment.production && !environment.enableLogging) {
+    // Override console methods
+    console.log = () => {}; // Disable console.log
+    console.warn = () => {}; // Optional: Disable console.warn
+    console.info = () => {}; // Optional: Disable console.info
+    console.debug = () => {}; // Optional: Disable console.debug
+    console.error = console.error; // Keep console.error (Optional, can be disabled too)
+  }
+
+bootstrapApplication(AppComponent, appConfig)
+    .catch(err => console.error(err));
