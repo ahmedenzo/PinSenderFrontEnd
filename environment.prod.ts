a29ece0f842cc4df67f679707e853693c5d7c097
ahmedenzo <115672994+ahmedenzo@users.noreply.github.com>
2025-01-22 15:36:50 +0100

    Update environment.prod.ts

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://${window.location.hostname}/api', // Use the current hostname dynamically
-  brokerURL: 'wss://${window.location.hostname}/ws', // Use the current hostname dynamically for WebSocket
+  apiUrl: `${window.location.origin}/api`, // Uses the current origin (http or https)
+  brokerURL: `${window.location.origin.replace(/^http/, 'ws').replace(/^https/, 'wss')}/ws`, // Ensures wss for https
   enableLogging: false,
 };
