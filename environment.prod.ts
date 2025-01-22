a3e837cba6db6ec0b6dfa791be00e0ef91feda85
ahmedenzo <115672994+ahmedenzo@users.noreply.github.com>
2025-01-22 14:23:07 +0100

    Update environment.prod.ts

--- environment.prod.ts
+++ environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://172.17.2.61/api', // Use the WAF's IP address with HTTPS
-  brokerURL: 'wss://172.17.2.61/ws', // Use the WAF's IP address with WSS for WebSocket
+  brokerURL: 'ws://172.17.2.61/ws', // Use the WAF's IP address with WSS for WebSocket
   enableLogging: false,
 };
